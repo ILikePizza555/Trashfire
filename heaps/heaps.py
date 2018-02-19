b3930a34a032450c7dcf0d01817fdf82380a3b8d
Isaac Avram <avrisaac555@gmail.com>
2018-02-19 14:33:27 -0800

    Fixed a few bugs

--- heaps/heaps.py
+++ heaps/heaps.py
@@ -15,9 +15,7 @@ class BinaryHeap():
         self.data = []
 
     def __repr__(self):
-        l = len(self.data)
-        m = self.get_max()[0]
-        return "BinaryHeap size {l} max: {m}"
+        return f"BinaryHeap size {len(self.data)} max: {self.get_max()[0]} - {self.data}"
     
     def _get_parent_index(self, index: int) -> int:
         return int(floor(index / 2))
@@ -26,11 +24,13 @@ class BinaryHeap():
         return self.data[self._get_parent_index(index)]
 
     def _get_children_index(self, index: int) -> Tuple[int, int]:
-        return index * 2, index * 2 + 1
+        a = index * 2 + 1 if (index * 2 + 1) < len(self.data) else None
+        b = index * 2 + 2 if (index * 2 + 2) < len(self.data) else None
+        return a, b
 
-    def _get_children(self, index: int) -> Tuple["BinaryHeap.Node", "BinaryHeap.Node"]:
+    def get_children(self, index: int) -> Tuple["BinaryHeap.Node", "BinaryHeap.Node"]:
         a, b = self._get_children_index(index)
-        return self.data[a], self.data[b]
+        return self.data[a] if a is not None else None, self.data[b] if b is not None else None
 
     def _is_leaf(self, index: int):
         return 2 * index > len(self.data)
@@ -40,7 +40,7 @@ class BinaryHeap():
             return
         
         n = self.data[index]
-        c1, c2 = self._get_children(index)
+        c1, c2 = self.get_children(index)
         if c1.key < n.key and c2.key < n.key:
             return
         
@@ -55,6 +55,8 @@ class BinaryHeap():
             self._heapify(c2_i)
 
     def get_max(self):
+        if not self.data:
+            return None, None
         return self.data[0].key, self.data[0].value
 
     def insert(self, key, item):
@@ -65,7 +67,8 @@ class BinaryHeap():
         # Fix the heap
         index = len(self.data) - 1
         parent_index = self._get_parent_index(index)
-        while self.data[parent_index].key > n.key:
+
+        while self.data[parent_index].key < n.key:
             swap(self.data, index, parent_index)
 
             # Recalculate indexes
