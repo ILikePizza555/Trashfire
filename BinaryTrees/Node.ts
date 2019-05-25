201b6d446bd30bad845d71b81b30a672bd410f35
Izzy <avrisaac555@gmail.com>
2019-05-24 20:16:17 -0700

    Redo

--- BinaryTrees/Node.ts
+++ BinaryTrees/Node.ts
@@ -1,20 +1,3 @@
 namespace DataStructures {
-    interface TreeNode<T>{
-        readonly value: T;
-        readonly children: TreeNode<T>[];
-
-        /**
-         * Inserts an item into the tree.
-         * @param item 
-         */
-        insert(item: T | TreeNode<T>): void;
-
-        /**
-         * Removes child i from the tree.
-         * @param i 
-         */
-        deleteChild(i: number): void;
-    }
-
-
+    
 }
